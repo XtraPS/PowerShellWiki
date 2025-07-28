2d4dcdbb6c443b0c6499a9861e2bfbf214e222c2
XmoXPosh <sdmock@gmail.com>
2025-07-27 21:32:44 -0500

    ..

--- Test-Validation.ps1
+++ Test-Validation.ps1
@@ -1,17 +1,21 @@
 
 
-("Server01", "Server02", "Server03") | Out-File -FilePath .\servers.txt
 function Test-Validation {
     param(
-        [ValidateSet("System", "Application", "Security")]
+        [ValidateSet("System", "Application")]
         [string]$LogName,
         [ValidateRange(1, 100)]
         [int]$MaxEvents,
-        [ValidateScript({ $_ -in Get-Content .\servers.txt })]
-        [ArgumentCompleter({ Get-Content .\servers.txt })]
+        [ValidateScript({ $_ -in (Get-WinEvent -ListProvider * | where { $_. }) })]
+        [ArgumentCompleter({ Get-Content .\Computers.txt })]
         [string]$ComputerName
     )
 
     Get-WinEventLog -LogName $LogName -Newest $MaxEvents -ComputerName $ComputerName
     
 }
+
+# Get windows event provider names
+
+# Get 2 hours ago
+$TwoHoursAgo = (Get-Date).AddHours(-2)
